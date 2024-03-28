48e05067d84ba471a2e1cd33bd022eae139e0a6b
Reshetov Ivan <reshetov.prog@gmail.com>
2024-03-28 21:16:58 +0300

    create structure

---
+++ storage/src/main/java/com/example/storage/entities/Supply.java
@@ -0,0 +1,17 @@
+package com.example.storage.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="supplies")
+public class Supply {
+}
